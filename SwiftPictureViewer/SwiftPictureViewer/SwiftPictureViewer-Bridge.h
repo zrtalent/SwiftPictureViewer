38351e4e2f7a029a55103d0b955214f49d99c876
大任 <zrtalent@gmail.com>
2015-03-11 20:03:38 +0800

    "Swift&OC桥接，加入自己的网络框架"

---
+++ SwiftPictureViewer/SwiftPictureViewer/SwiftPictureViewer-Bridge.h
@@ -0,0 +1,17 @@
+//
+//  SwiftPictureViewer-Bridge.h
+//  SwiftPictureViewer
+//
+//  Created by Zr on 15/3/11.
+//  Copyright (c) 2015年 Tarol. All rights reserved.
+//
+
+#import <CommonCrypto/CommonCrypto.h>
+#import "SVProgressHUD.h"
+
+// 提示：以下写法，是在 C/C++ 中避免 .h 文件被重复 #include
+#ifndef SwiftPictureViewer_SwiftPictureViewer_h
+#define SwiftPictureViewer_SwiftPictureViewer_h
+
+
+#endif
